3ae0d73bb97c5977246c74ce8adf859753fb9124
Khalil <khalilayache@gmail.com>
2019-07-27 10:34:24 -0300

    Add Game logic

--- Class_4/TicToe/app/src/main/java/dev/khalil/tictoe/views/game/GameViewModel.kt
+++ Class_4/TicToe/app/src/main/java/dev/khalil/tictoe/views/game/GameViewModel.kt
@@ -1,2 +1,33 @@
 package dev.khalil.tictoe.views.game
 
+import androidx.databinding.ObservableArrayMap
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import dev.khalil.tictoe.models.Cell
+import dev.khalil.tictoe.models.Game
+import dev.khalil.tictoe.models.Player
+import dev.khalil.tictoe.utils.StringUtility.stringFromNumbers
+
+class GameViewModel : ViewModel() {
+
+    lateinit var cells: ObservableArrayMap<String, String>
+    private lateinit var game: Game
+    val winner: LiveData<Player>
+        get() = game.winner
+
+    fun init(player1: String, player2: String) {
+        game = Game(player1, player2)
+        cells = ObservableArrayMap()
+    }
+
+    fun onClickedCellAt(row: Int, column: Int) {
+        if (game.cells[row][column] == null) {
+            game.cells[row][column] = Cell(game.currentPlayer)
+            cells[stringFromNumbers(row, column)] = game.currentPlayer.value
+            if (game.hasGameEnded())
+                game.reset()
+            else
+                game.switchPlayer()
+        }
+    }
+}

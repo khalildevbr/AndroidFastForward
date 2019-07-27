bdb74f6e8f72e3ab0783cfd6a48ff7ee22572f7c
Khalil <khalilayache@gmail.com>
2019-07-27 10:18:07 -0300

    Remove MainActivity and add GameActivity

--- Class_4/TicToe/app/src/main/java/dev/khalil/tictoe/views/game/GameActivity.kt
+++ Class_4/TicToe/app/src/main/java/dev/khalil/tictoe/views/game/GameActivity.kt
@@ -1,12 +1,13 @@
-package dev.khalil.tictoe
+package dev.khalil.tictoe.views.game
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.khalil.tictoe.R
 
-class MainActivity : AppCompatActivity() {
+class GameActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_game)
     }
 }
